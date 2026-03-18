206caa11200edffcc3e24b2aa26dbc4cd40c2c53
Rin <carinamgarrity@gmail.com>
2026-03-18 16:16:33 -0600

    feat: initial GUI implementation exploration
    Functional, but needs refactor and cleanup

--- script.js
+++ script.js
@@ -2,277 +2,275 @@
 //                         GAME PROGRAM
 // --------------------------------------------------------------------------------
 
+// *************************************************************
+//         DOM ELEMENTS
+// *************************************************************
+
+//  + + + THROW HANDS + + +
+const choiceButtons = document.querySelectorAll('#btn-choice-container button');
+
+//  + + + RPS ARENA + + +
+const playerChoiceText = document.getElementById('player-choice-text');
+const playerRoundResults = document.getElementById('player-round-results');
+
+//  + + + SCORE BOARD + + +
+const scoreBoard = document.getElementById('score-board');
+const roundNumber = document.getElementById('round-number');
+
+//  + + + GAME PROGRESS + + +
+const gameProgressHeader = document.getElementById('game-progress-header');
+const gameProgressText = document.getElementById('game-progress-text');
+const newGameButton = document.getElementById('btn-play-again');
 
 // *************************************************************
-//         gameInit, playGame: BEGIN GAME
+//         GAME STATE
 // *************************************************************
 
+const gameState = {
+  userScore: 0,
+  /*--- This will keep track of the user score, which will be used to determine the game winner ---*/
 
+  computerScore: 0,
+  /*--- This will keep track of the computer score, which will be used to determine the game winner ---*/
 
-function playGame() {
+  tiesScore: 0,
 
-    const gameState = {
+  roundCounter: 0,
+  /*--- This will keep track of the rounds played, which will be used to determine when the game is over ---*/
+};
 
-        userScore: 0,
-        /*--- This will keep track of the user score, which will be used to determine the game winner ---*/
+// *************************************************************
+//         BEGIN GAME
+// *************************************************************
+newGameButton.disabled = true;
+newGameButton.classList.add('disabled-button');
+choiceButtons.disabled = false;
+choiceButtons.forEach((choiceButton) => {
+  choiceButton.classList.remove('disabled-button');
+});
 
-        computerScore: 0,
-        /*--- This will keep track of the computer score, which will be used to determine the game winner ---*/
+choiceButtons.forEach((button) => {
+  // RETURNS userChoice in an uppercase string containing either "ROCK", "PAPER", or "SCISSORS"
+  button.addEventListener('click', (e) => {
+    if (gameState.roundCounter < 5) {
+      ++gameState.roundCounter;
 
-        roundCounter: 0,
-        /*--- This will keep track of the rounds played, which will be used to determine when the game is over ---*/
-    };
+      /* ----- Updates the status text in the Game Status area. ----- */
+      gameProgressText.textContent = '[game in progress...]';
 
-    while (gameState.roundCounter < 5) {
+      /* ----- This function gets the user's hand as ROCK, PAPER, or SCISSORS. ----- */
+      const userChoice = e.target.textContent;
+      console.log('userChoice initial = ', userChoice);
 
-        ++gameState.roundCounter;
+      // RETURNS computerChoice assigned with "ROCK", "PAPER", or "SCISSORS"
+      let computerChoiceResult = getComputerChoice();
 
-        let computerChoiceResult = getComputerChoice()
-        // RETURNS computerChoice assigned with "ROCK", "PAPER", or "SCISSORS"
+      let roundWinner = determineRoundWinner(userChoice, computerChoiceResult);
 
-        let userChoiceInput = getUserChoiceRPS();
-        // RETURNS userChoice in an uppercase string containing either "ROCK", "PAPER", or "SCISSORS"
+      // Compares user and computer selections and determines the winner of the round.
+      // RETURNS roundWinner with value "TIE", userRoundWinner, or computerRoundWinner
+      determineRoundWinner(userChoice, computerChoiceResult);
 
-        let roundWinner = determineRoundWinner(userChoiceInput, computerChoiceResult)
-        // Compares user and computer selections and determines the winner of the round.
-        // RETURNS roundWinner with value "TIE", userRoundWinner, or computerRoundWinner
+      // Increases the score of the one who won the round
+      // RETURNS text: tell the user the scores of both players
+      increaseWinnerScore(roundWinner, gameState);
 
-        announceRoundWinner(roundWinner);
+      // Announces the winner of this round
+      // RETURNS text: tell user it's a tie, or that they won, or that the computer won
+      displayArenaText(roundWinner, userChoice, computerChoiceResult);
+      displayRoundScores(gameState);
+
+      if (gameState.roundCounter === 5) {
+        // displayGameWinner(gameState);
         // Announces the winner of this round
         // RETURNS text: tell user it's a tie, or that they won, or that the computer won
 
-        increaseWinnerScore(roundWinner, gameState);
-        // Increases the score of the one who won the round
-        // RETURNS text: tell the user the scores of both players
+        // maybe change the brightness of the button backgrounds to make it more obvious.
+        choiceButtons.disabled = true;
+        choiceButtons.forEach((choiceButton) => {
+          choiceButton.classList.add('disabled-button');
+        });
+        newGameButton.disabled = false;
+        newGameButton.classList.remove('disabled-button');
 
-    } // END OF WHILE LOOP
+        displayGameWinner(gameState);
+      }
+    } // end of IF statement
+  });
+});
 
-        announceGameWinner(gameState);
-        // Announces the winner of this round
-        // RETURNS text: tell user it's a tie, or that they won, or that the computer won
+// *************************************************************
+//         NEW GAME
+// *************************************************************
 
-        let displayScore = JSON.stringify(gameState, null, 4);
-    
-        window.confirm(`Current score: ${displayScore}`);
+// + + + + + NEW GAME BUTTON + + + + +
+// clear printed data from the containers
+newGameButton.addEventListener('click', () => {
+  // change each gameState object back to 0
+  gameState.userScore = 0;
+  gameState.computerScore = 0;
+  gameState.roundCounter = 0;
 
-    return gameOver();
+  // RPS ARENA: change each DOM element back to default text
+  playerChoiceText.textContent =
+    'Click ROCK, PAPER, or SCISSORS to start the game.';
+  playerRoundResults.textContent = 'Game will end after five rounds.';
 
-}
+  // SCORE BOARD: change each DOM element back to default text
+  scoreBoard.textContent = 'User score: 0 | Computer score: 0 | Ties: 0';
+  roundNumber.textContent = 'Round number: 0';
 
-function gameOver() {
-    
-    const newGameInput = prompt("Do you want to play a new game? Enter YES or NO.").toUpperCase();
+  // GAME PROGRESS: change each DOM element back to default text
+  gameProgressHeader.textContent = 'Game not started.';
+  gameProgressText.textContent = '[waiting for user...]';
 
-    /*--- Short circuit  ---*/
-    if (newGameInput === "" || newGameInput === null) {
-    newGameInput = window.prompt("You didn't enter anything, please try again. Do you want to play a new game? Enter YES or NO.");
-    return gameOver(newGameInput);
-    }
-
-
-    if (newGameInput !== "YES" && newGameInput !== "NO") {
-    newGameInput = prompt("Your answer was invalid, please try again. Do you want to play a new game? Enter YES or NO.");
-    return gameOver(newGameInput);
-    }
-
-    if (newGameInput === "NO") {
-        return window.confirm("Thank you for playing!");
-    }
-
-    return playGame();
-}
-
-
-
+  // choiceButtons.classList.add('disabled-button'); //in a loop
+  choiceButtons.disabled = false;
+  newGameButton.disabled = true;
+});
 
 // --------------------------------------------------------------------------------
 //                         BELOW: UTILITY FUNCTIONS
 // --------------------------------------------------------------------------------
 
-
 // *************************************************************
 //         GET COMPUTER CHOICE
 // *************************************************************
 
-
 function getComputerChoice() {
-/*----- This will determine the computerChoice for the round. It will randomize a number from 1 to 3, which will output ROCK, PAPER, or SCISSORS respectively -----*/
+  /*----- This will determine the computerChoice for the round. It will randomize a number from 1 to 3, which will output ROCK, PAPER, or SCISSORS respectively -----*/
 
-    const number = Math.floor(Math.random() * 3) + 1;
-    let computerChoice = null;
+  const number = Math.floor(Math.random() * 3) + 1;
 
-    if (number === 1) {
-        return computerChoice = "ROCK";
-    }
-
-    else if (number === 2) {
-        return computerChoice = "PAPER";
-    }
-
-    else if (number === 3) {
-        return computerChoice = "SCISSORS";
-    }
-
-    else {
-        const errorMessage = window.confirm("There was an error generating the computer's choice");
-        return errorMessage;
-    }
+  if (number === 1) {
+    return 'ROCK';
+  } else if (number === 2) {
+    return 'PAPER';
+  } else {
+    return 'SCISSORS';
+  }
 }
-
-
-
-// *************************************************************
-//         GET USER CHOICE - RPS (ROCK PAPER SCISSORS)
-// *************************************************************
-
-
-function getUserChoiceRPS() {
-/* ----- This function GETS the user input ROCK, PAPER, or SCISSORS. ----- */
-
-    let rpsInput = window.prompt("What do you choose, ROCK, PAPER, or SCISSORS?");
-    
-    let userChoice = rpsInput.toUpperCase(); 
-
-    if (userChoice === "" || userChoice === null) {
-        window.confirm("Your answer is invalid, please try again.");
-        return rpsInput = getUserChoiceRPS(rpsInput); 
-    }
-
-    if (userChoice !== "ROCK" && userChoice !== "PAPER" && userChoice !== "SCISSORS") {
-        window.confirm("Your answer is invalid, please try again.");
-        return rpsInput = getUserChoiceRPS(rpsInput); 
-    }
-
-    return userChoice;
-
-}
-
-
 
 // *************************************************************
 //         DETERMINE ROUND WINNER
 // *************************************************************
 
-
 function determineRoundWinner(userChoiceInput, computerChoiceResult) {
-/*----- This will take the user and computer round results from computerChoice and userChoice, and determine the winner of the round -----*/
+  /*----- This will take the user and computer round results from computerChoice and userChoice, and determine the winner of the round -----*/
 
-    const userChoice = userChoiceInput;
-    const computerChoice = computerChoiceResult;
+  const userChoice = userChoiceInput;
+  const computerChoice = computerChoiceResult;
 
-    let roundWinner = null;
+  // HANDLING THE TIE (short-circuit)
+  if (userChoice === computerChoice) {
+    return 'TIE';
+  }
 
-
-    // HANDLING THE TIE (short-circuit)
-    if (userChoice === computerChoice) {
-        return roundWinner = "TIE";
-    }
-
-    // ROCK BEATS SCISSORS
-    if  (userChoice === "ROCK" && computerChoice === "SCISSORS") {
-        roundWinner = "USER";
-    }
-    else if (userChoice === "SCISSORS" && computerChoice === "ROCK") {
-        roundWinner = "COMPUTER";
-
+  // ROCK BEATS SCISSORS
+  if (userChoice === 'ROCK' && computerChoice === 'SCISSORS') {
+    return 'USER';
+  } else if (userChoice === 'SCISSORS' && computerChoice === 'ROCK') {
+    return 'COMPUTER';
 
     // SCISSORS BEATS PAPER
-    }
-    else if (userChoice === "SCISSORS" && computerChoice === "PAPER") {
-        roundWinner = "USER";
-    }
-    else if (userChoice === "PAPER" && computerChoice === "SCISSORS") {
-        roundWinner = "COMPUTER";
-    }
+  } else if (userChoice === 'SCISSORS' && computerChoice === 'PAPER') {
+    return 'USER';
+  } else if (userChoice === 'PAPER' && computerChoice === 'SCISSORS') {
+    return 'COMPUTER';
+  }
 
-
-    // PAPER BEATS ROCK
-    else if (userChoice === "PAPER" && computerChoice === "ROCK") {
-        roundWinner = "USER";
-    }
-    else if (userChoice === "ROCK" && computerChoice === "PAPER") {
-        roundWinner = "COMPUTER";
-    }
-
-    return roundWinner;
+  // PAPER BEATS ROCK
+  else if (userChoice === 'PAPER' && computerChoice === 'ROCK') {
+    return 'USER';
+  } else if (userChoice === 'ROCK' && computerChoice === 'PAPER') {
+    return 'COMPUTER';
+  }
 }
-
-
-
-// *************************************************************
-//         ANNOUNCE ROUND WINNER
-// *************************************************************
-
-
-function announceRoundWinner(roundWinner) {
-/*----- This will take value of roundWinner from determineRoundWinner and announce the winner. -----*/
-
-    switch (roundWinner) {
-
-        case "TIE":
-            return window.confirm("It's a tie!");
-
-        case "USER":
-            return window.confirm("You won this round!");
-
-        case "COMPUTER":
-            return window.confirm("The computer won this round!");
-    }
-
-}
-
-
 
 // *************************************************************
 //         INCREASE WINNER SCORE
 // *************************************************************
 
-
 function increaseWinnerScore(roundWinner, gameState) {
-/*----- This will take the results from determineRoundWinner and increment their score. -----*/
+  /*----- This will take the results from determineRoundWinner and increment their score. -----*/
 
+  /*--- short circuit: we don't need to increase the score on a tie ---*/
+  if (roundWinner === 'TIE') {
+    ++gameState.tiesScore;
+  } else if (roundWinner === 'USER') {
+    ++gameState.userScore;
+  } else {
+    ++gameState.computerScore;
+  }
 
-    /*--- short circuit: we don't need to increase the score on a tie ---*/
-    if (roundWinner === "TIE") {
-        return;
-    }
-
-    else if (roundWinner === "USER") {
-        ++gameState.userScore;
-    }
-    else {
-        ++gameState.computerScore;
-    }
-
-    return;
-
+  return;
 }
 
+// *************************************************************
+//         ANNOUNCE ROUND WINNER (ARENA)
+// *************************************************************
 
+function displayArenaText(roundWinner, userChoice, computerChoiceResult) {
+  /*----- This will take value of roundWinner from determineRoundWinner and announce the winner by printing the text in the RPS ARENA. -----*/
+  const userHand = userChoice;
+  const computerHand = computerChoiceResult;
+
+  switch (roundWinner) {
+    case 'TIE':
+      playerChoiceText.textContent = "It's a tie!";
+      break;
+
+    case 'USER':
+      playerChoiceText.textContent = 'You won this round!';
+      break;
+
+    case 'COMPUTER':
+      playerChoiceText.textContent = 'The computer won this round!';
+      break;
+  }
+  playerRoundResults.textContent =
+    'You chose ' + userHand + ' and the computer chose ' + computerHand;
+}
+
+// *************************************************************
+//         DISPLAY ROUND SCORES (SCORE BOARD)
+// *************************************************************
+
+function displayRoundScores(gameState) {
+  /*----- This will take value of roundWinner from determineRoundWinner and announce the winner by printing the text in the RPS ARENA. -----*/
+  const currentUserScore = gameState.userScore;
+  const currentComputerScore = gameState.computerScore;
+  const currentTiesScore = gameState.tiesScore;
+  const currentRound = gameState.roundCounter;
+
+  scoreBoard.textContent =
+    'User score: ' +
+    currentUserScore +
+    ' | Computer score: ' +
+    currentComputerScore +
+    ' | Ties: ' +
+    currentTiesScore;
+
+  roundNumber.textContent = 'Round: ' + currentRound;
+}
 
 // *************************************************************
 //         END OF 5 ROUNDS: ANNOUNCE GAME WINNER
 // *************************************************************
 
+function displayGameWinner(gameState) {
+  /*----- This will announce the winner of the game. -----*/
+  const userScore = gameState.userScore;
+  const computerScore = gameState.computerScore;
 
-function announceGameWinner(gameState) {
-/*----- This will announce the winner of the game. -----*/
-
-    const userScore = gameState.userScore;
-    const computerScore = gameState.computerScore;
-
-        if (userScore === computerScore) {
-            return window.confirm("The game ends in a tie!");
-        }
-        else if (userScore < computerScore) {
-            return window.confirm("A winner is you! (you won the game!)");
-        }
-        else {
-            return window.confirm("Bad end. (you lost the game)");
-        }
+  if (userScore === computerScore) {
+    gameProgressHeader.textContent = "YOU'RE EQUAL IN STRENGTH.";
+    return (gameProgressText.textContent = '[the game is tied]');
+  } else if (userScore > computerScore) {
+    gameProgressHeader.textContent = 'A WINNER IS YOU!';
+    return (gameProgressText.textContent = '[you won the game]');
+  } else {
+    gameProgressHeader.textContent = 'BAD END.';
+    return (gameProgressText.textContent = '[you lost the game]');
+  }
 }
-
-
-// On load, start game
-
-playGame()
